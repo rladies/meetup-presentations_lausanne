55d7fd7c63de6faf9a8866a686617d5cf7e963ec
Sina Rueeger <sina.rueeger@gmail.com>
2018-06-06 16:34:08 +0200

    tidying up

--- 20180607_thundeR/3_tidytuesday/get-started-tidytuesday.R
+++ 20180607_thundeR/3_tidytuesday/get-started-tidytuesday.R
@@ -1,26 +1,36 @@
 ## Prep for meetup #4 of R-Ladies Lausanne
 ## ----------------------------------------
 
+## some reading
+## ----------------
 ## R4DS community: http://www.rfordatasci.com/
 ## tidytuesday on github: https://github.com/rfordatascience/tidytuesday
 
 
 ## load tidyverse
 ## ---------------
+if(!require(tidyverse))
+    install.packages("tidyverse")
 library(tidyverse)
 
+
 ## download data to a data/ folder
+## (this can be automated in R too)
 ## -------------------------------
+## 1. Right next to this R file, create a folder called "data"
 ## 1. download https://github.com/rfordatascience/tidytuesday/blob/master/data/week10_biketown.zip
-## 2. download https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week9_comic_characters.csv
+## 2. unzip zip folder
+## 3. download https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week9_comic_characters.csv
+## 4. copy both csv files in the data folder. 
 
 
-##  week #9
+##  WEEK #9 COMIC CHARACTERS
 ## -------------
+
 ## article: https://fivethirtyeight.com/features/women-in-comic-books/
 dat.comic <- read_csv("data/week9_comic_characters.csv")
 
-## week #10
+## WEEK #10 BIKETOWN
 ## -------------
 
 ## article: https://www.biketownpdx.com/system-data
