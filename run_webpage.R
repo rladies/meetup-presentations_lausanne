07021328ed9bb4f66760d2d12772ba554f45dfb6
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-02 16:06:58 +0200

    removed disqus and google

--- run_webpage.R
+++ run_webpage.R
@@ -22,16 +22,18 @@ blogdown::hugo_build()
 ## 3) go to a terminal
 ## ---------------------------
 
-## a) commit changes to github 
-## cd /Users/admin/Documents/Projects/rladies/rladieslausanne.github.io
-## git add -f *
-## git commit -a -m 'your commit message'
-## git push
+comment <- "'removed disqus and google'"
 
 ## b) commit changes of webpage folder
-## go to terminal
-## cd /Users/admin/Documents/Projects/rladies/meetup-presentations_lausanne/webpage
-## git add -f *
-## git commit -a -m 'your commit message'
-## git push
+## add files system("git add ...")
+system(paste0("git commit -a -m ", comment))
+system("git push")
+
+
+## a) commit changes to github 
+setwd("/Users/admin/Documents/Projects/rladies/rladieslausanne.github.io/")
+system("git add -f *")
+system(paste0("git commit -a -m ", comment))
+system("git push")
+
 
