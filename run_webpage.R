bafbe2b79f9faff4f77d6aaf83ca87a3e40fc7de
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-03 11:25:53 +0200

    adding august newsletter draft

--- run_webpage.R
+++ run_webpage.R
@@ -23,7 +23,7 @@ blogdown::hugo_build()
 ## ---------------------------
 
 path <- getwd()
-comment <- "'adding slides'"
+comment <- "'adding august newsletter draft'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
