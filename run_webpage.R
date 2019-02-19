4ef39ef4dab25addcd3c7b028109730d4da0d8dc
n-mounier <mounier.ninon@gmail.com>
2019-02-19 10:58:26 +0100

    re-add Liza interview

--- run_webpage.R
+++ run_webpage.R
@@ -23,7 +23,7 @@ blogdown::hugo_build()
 ## ---------------------------
 
 path <- getwd()
-comment <- "'add current chapters'"
+comment <- "'re-add Liza's interview"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
